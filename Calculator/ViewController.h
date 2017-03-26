76fb939d3711eb695c5e7b72ec52d2b4bbb9bdf8
Aaron Wright <aaron.c.wright@icloud.com>
2017-03-26 02:55:29 -0500

    Initial Commit

---
+++ Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by A.C. Wright Design on 3/26/17.
+//  Copyright © 2017 A.C. Wright Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Calculator/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Calculator
+//
+//  Created by A.C. Wright Design on 3/26/17.
+//  Copyright © 2017 A.C. Wright Design. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
